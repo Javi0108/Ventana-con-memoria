b0cd65966a9f11e8d39992acdbe18159cbde6513
Javi0108 <59823740+Javi0108@users.noreply.github.com>
2020-11-24 18:49:42 +0000

    Add files via upload

---
+++ src/main/java/dad/javafx/ventanaConMemoria/Main.java
@@ -0,0 +1,11 @@
+package dad.javafx.ventanaConMemoria;
+
+import dad.javafx.ventanaConMemoria.mvc.App;
+
+public class Main {
+
+	public static void main(String[] args) {
+		App.main(args);
+	}
+
+}
